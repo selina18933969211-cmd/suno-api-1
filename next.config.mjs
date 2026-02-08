4530a64819fc4a5a7ed2f4d688b3127fb5a6811c
selina18933969211-cmd <selina18933969211@gmail.com>
2026-02-08 19:05:03 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['rebrowser-playwright-core'],
+  },
 };
 
 export default nextConfig;
